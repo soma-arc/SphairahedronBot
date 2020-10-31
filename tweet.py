72a4376441c8d72546b8134d2434058dd94e1175
soma_arc <somaarcr@gmail.com>
2020-10-31 22:43:03 +0900

    Add a getting tweet example

---
+++ tweet.py
@@ -0,0 +1,16 @@
+import tweepy
+
+consumer_key = 'xxx'
+consumer_secret = 'xxx'
+
+access_token = 'xxx'
+access_token_secret = 'xxx'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
