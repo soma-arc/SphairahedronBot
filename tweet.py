2a271f6646dafab8c036454e4e62bcad036d8cf5
soma_arc <somaarcr@gmail.com>
2020-11-01 15:55:06 +0900

    Upload multiple images

--- tweet.py
+++ tweet.py
@@ -11,6 +11,14 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+#api.update_status('test tweet');
+#api.update_with_media(status = 'test tweet with media', filename = 'flame.png')
+
+filenames = ['flame.png', 'loxoExponential.png',
+             'fractalFlame.png', 'kleinianWalker.png']
+media_ids = []
+for filename in filenames:
+        res = api.media_upload(filename)
+        media_ids.append(res.media_id)
+
+api.update_status(status='four images!', media_ids=media_ids)
